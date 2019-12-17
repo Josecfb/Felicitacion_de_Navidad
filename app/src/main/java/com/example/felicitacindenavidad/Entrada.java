033b8224ef82c8467cbda3e84b2cd16f7ba91db2
“Jose <cursillos2002@gmail.com>
2019-12-17 13:19:10 +0100

    dos

--- app/src/main/java/com/example/felicitacindenavidad/Entrada.java
+++ app/src/main/java/com/example/felicitacindenavidad/Entrada.java
@@ -27,33 +27,35 @@ public class Entrada extends AppCompatActivity implements Animation.AnimationLis
         TextView texto_entrada=findViewById(R.id.texto_entrada);
         Animation animacion= AnimationUtils.loadAnimation(this,R.anim.texto_entrada);
         texto_entrada.startAnimation(animacion);
-
-
-        ImageView adorno1=findViewById(R.id.adorno1);
-        ImageView adorno2=findViewById(R.id.adorno2);
-        ImageView adorno3=findViewById(R.id.adorno3);
-        ImageView adorno4=findViewById(R.id.adorno4);
-        ImageView adorno5=findViewById(R.id.adorno5);
-        ImageView adorno6=findViewById(R.id.adorno6);
-        ImageView adorno7=findViewById(R.id.adorno7);
-        ImageView adorno8=findViewById(R.id.adorno8);
-        ImageView adorno9=findViewById(R.id.adorno9);
-        ImageView adorno10=findViewById(R.id.adorno10);
+        ImageView papa=findViewById(R.id.papa);
+        Animation escalar=AnimationUtils.loadAnimation(this,R.anim.escalar);
+        papa.startAnimation(escalar);
+        ImageView[] adorno=new ImageView[10];
+        adorno[0]=findViewById(R.id.adorno1);
+        adorno[1]=findViewById(R.id.adorno2);
+        adorno[2]=findViewById(R.id.adorno3);
+        adorno[3]=findViewById(R.id.adorno4);
+        adorno[4]=findViewById(R.id.adorno5);
+        adorno[5]=findViewById(R.id.adorno6);
+        adorno[6]=findViewById(R.id.adorno7);
+        adorno[7]=findViewById(R.id.adorno8);
+        adorno[8]=findViewById(R.id.adorno9);
+        adorno[9]=findViewById(R.id.adorno10);
         Animation caer1=AnimationUtils.loadAnimation(this,R.anim.caer1);
         Animation caer2=AnimationUtils.loadAnimation(this,R.anim.caer2);
         Animation caer3=AnimationUtils.loadAnimation(this,R.anim.caer3);
         Animation caer4=AnimationUtils.loadAnimation(this,R.anim.caer4);
         Animation caer5=AnimationUtils.loadAnimation(this,R.anim.caer5);
-        adorno1.startAnimation(caer1);
-        adorno10.startAnimation(caer1);
-        adorno2.startAnimation(caer2);
-        adorno9.startAnimation(caer2);
-        adorno3.startAnimation(caer3);
-        adorno8.startAnimation(caer3);
-        adorno4.startAnimation(caer4);
-        adorno7.startAnimation(caer4);
-        adorno5.startAnimation(caer5);
-        adorno6.startAnimation(caer5);
+        adorno[0].startAnimation(caer1);
+        adorno[9].startAnimation(caer1);
+        adorno[1].startAnimation(caer2);
+        adorno[8].startAnimation(caer2);
+        adorno[2].startAnimation(caer3);
+        adorno[7].startAnimation(caer3);
+        adorno[3].startAnimation(caer4);
+        adorno[6].startAnimation(caer4);
+        adorno[4].startAnimation(caer5);
+        adorno[5].startAnimation(caer5);
 
         animacion.setAnimationListener(this);
     }
@@ -65,7 +67,8 @@ public class Entrada extends AppCompatActivity implements Animation.AnimationLis
 
     @Override
     public void onAnimationEnd(Animation animation) {
-        Intent intent = new Intent(Entrada.this, Felicitacion.class);
+        Intent intent;
+        intent = new Intent(this, Felicitacion.class);
         startActivity(intent);
         finish();
     }
--- app/src/main/java/com/example/felicitacindenavidad/Felicitacion.java
+++ app/src/main/java/com/example/felicitacindenavidad/Felicitacion.java
@@ -1,8 +1,6 @@
 package com.example.felicitacindenavidad;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.animation.Animation;
@@ -10,68 +8,93 @@ import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import static java.util.Objects.requireNonNull;
+
 public class Felicitacion extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_felicitacion);
-
-
-        TextView feliz=  findViewById(R.id.feliz);
-        TextView navidad=findViewById(R.id.navidad);
+        requireNonNull(getSupportActionBar()).hide();
         MediaPlayer mediaPlayer=MediaPlayer.create(this,R.raw.jinglebellrock);
         mediaPlayer.start();
+        TextView[] l=new TextView[12];
+        l[0]=  findViewById(R.id.l1);
+        l[1]=  findViewById(R.id.l2);
+        l[2]=  findViewById(R.id.l3);
+        l[3]=  findViewById(R.id.l4);
+        l[4]=  findViewById(R.id.l5);
+        l[5]=  findViewById(R.id.l6);
+        l[6]=  findViewById(R.id.l7);
+        l[7]=  findViewById(R.id.l8);
+        l[8]=  findViewById(R.id.l9);
+        l[9]=  findViewById(R.id.l10);
+        l[10]=  findViewById(R.id.l11);
+        l[11]=  findViewById(R.id.l12);
+        ImageView[] arbol=new ImageView[3];
+        arbol[0]=findViewById(R.id.arbol1);
+        arbol[1]=findViewById(R.id.arbol2);
+        arbol[2]=findViewById(R.id.arbol3);
+        Animation animacion= AnimationUtils.loadAnimation(this,R.anim.aparecer1);
+        for(int i=0;i<l.length;i++)
+            l[i].startAnimation(animacion);
+        for(int i=0;i<arbol.length;i++)
+            arbol[i].startAnimation(animacion);
 
-        Animation animacion= AnimationUtils.loadAnimation(this,R.anim.aparecer);
-
-        feliz.startAnimation(animacion);
-        navidad.startAnimation(animacion);
-
+        ImageView[] reno=new ImageView[10];
         Animation baile=AnimationUtils.loadAnimation(this,R.anim.baile);
-        ImageView reno1=findViewById(R.id.reno1);
-        ImageView reno2=findViewById(R.id.reno2);
-        ImageView reno3=findViewById(R.id.reno3);
-        ImageView reno4=findViewById(R.id.reno4);
-        ImageView reno5=findViewById(R.id.reno5);
-        ImageView reno6=findViewById(R.id.reno6);
+        reno[0]=findViewById(R.id.reno11);
+        reno[1]=findViewById(R.id.reno12);
+        reno[2]=findViewById(R.id.reno13);
+        reno[3]=findViewById(R.id.reno14);
+        reno[4]=findViewById(R.id.reno15);
+        reno[5]=findViewById(R.id.reno21);
+        reno[6]=findViewById(R.id.reno22);
+        reno[7]=findViewById(R.id.reno23);
+        reno[8]=findViewById(R.id.reno24);
+        reno[9]=findViewById(R.id.reno25);
+        for(int i=0;i<reno.length;i++)
+            reno[i].startAnimation(baile);
 
-        reno1.startAnimation(baile);
-        reno2.startAnimation(baile);
-        reno3.startAnimation(baile);
-        reno4.startAnimation(baile);
-        reno5.startAnimation(baile);
-        reno6.startAnimation(baile);
-
+        ImageView[] campana=new ImageView[12];
         Animation baile2=AnimationUtils.loadAnimation(this,R.anim.baile2);
-        ImageView campana1=findViewById(R.id.campana1);
-        ImageView campana2=findViewById(R.id.campana2);
-        ImageView campana3=findViewById(R.id.campana3);
-        ImageView campana4=findViewById(R.id.campana4);
-        ImageView campana5=findViewById(R.id.campana5);
+        campana[0]=findViewById(R.id.campana11);
+        campana[1]=findViewById(R.id.campana12);
+        campana[2]=findViewById(R.id.campana13);
+        campana[3]=findViewById(R.id.campana14);
+        campana[4]=findViewById(R.id.campana15);
+        campana[5]=findViewById(R.id.campana16);
+        campana[6]=findViewById(R.id.campana21);
+        campana[7]=findViewById(R.id.campana22);
+        campana[8]=findViewById(R.id.campana23);
+        campana[9]=findViewById(R.id.campana24);
+        campana[10]=findViewById(R.id.campana25);
+        campana[11]=findViewById(R.id.campana26);
 
-        campana1.startAnimation(baile2);
-        campana2.startAnimation(baile2);
-        campana3.startAnimation(baile2);
-        campana4.startAnimation(baile2);
-        campana5.startAnimation(baile2);
+        for(int i=0;i<campana.length;i++)
+            campana[i].startAnimation(baile2);
 
+        ImageView[] boy=new ImageView[6];
+        ImageView[] girl=new ImageView[6];
         Animation baile3=AnimationUtils.loadAnimation(this,R.anim.baile3);
-        ImageView boy1=findViewById(R.id.boy1);
-        ImageView boy2=findViewById(R.id.boy2);
-        ImageView boy3=findViewById(R.id.boy3);
-
-        boy1.startAnimation(baile3);
-        boy2.startAnimation(baile3);
-        boy3.startAnimation(baile3);
-
         Animation baile4=AnimationUtils.loadAnimation(this,R.anim.baile4);
-        ImageView girl1=findViewById(R.id.girl1);
-        ImageView girl2=findViewById(R.id.girl2);
-        ImageView girl3=findViewById(R.id.girl3);
+        boy[0]=findViewById(R.id.boy11);
+        boy[1]=findViewById(R.id.boy12);
+        boy[2]=findViewById(R.id.boy13);
+        boy[3]=findViewById(R.id.boy21);
+        boy[4]=findViewById(R.id.boy22);
+        boy[5]=findViewById(R.id.boy23);
+        girl[0]=findViewById(R.id.girl11);
+        girl[1]=findViewById(R.id.girl12);
+        girl[2]=findViewById(R.id.girl13);
+        girl[3]=findViewById(R.id.girl21);
+        girl[4]=findViewById(R.id.girl22);
+        girl[5]=findViewById(R.id.girl23);
+        for (int i=0;i<girl.length;i++){
+            boy[i].startAnimation(baile4);
+            girl[i].startAnimation(baile3);
 
-        girl1.startAnimation(baile4);
-        girl2.startAnimation(baile4);
-        girl3.startAnimation(baile4);
+        }
     }
 }
